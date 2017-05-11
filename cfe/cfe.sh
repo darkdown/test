bb5e4c450e2612e998f086a3b2a150fb4e86acc3
darkdown <darkdown@users.noreply.github.com>
2017-05-11 17:35:19 +0800

    Update cfe.sh

--- cfe/cfe.sh
+++ cfe/cfe.sh
@@ -8,23 +8,24 @@
 
 replace_encoding(){
 	from_encoding=`file -b --mime-encoding $1`
+	echo "[MSG] "$1 >> $log
+	echo "[MSG] from "$from_encoding" to "$to_encoding >> $log
 	if [ "$from_encoding" != "$to_encoding" ]
-	then 
-		echo "[MSG] from "$from_encoding" to "$to_encoding
+	then
 		iconv -f $from_encoding -t $to_encoding $1 > $1.utf-8
 		if [ $? -eq "1" ] 
 		then
 			rm $1.utf-8
-			echo "[MSG] failed."
+			echo "[MSG] failed." >> $log
 		else
 			diff $1 $1.utf-8
 			if [ $? -eq "0" ] 
 			then
 				rm $1.utf-8
-				echo "[MSG] same content." 
+				echo "[MSG] same content." >> $log
 			else 
 				cat $1.utf-8 > $1
-				echo "[MSG] ok." 
+				echo "[MSG] ok." >> $log
 			fi
 		fi
 	fi
@@ -32,5 +33,7 @@ replace_encoding(){
 
 export -f replace_encoding 
 export to_encoding=$1
+export log="`pwd`/cfe.log"
 
+echo . > $log
 find $2 -name $3 -exec bash -c 'replace_encoding "$0"' {} \;
